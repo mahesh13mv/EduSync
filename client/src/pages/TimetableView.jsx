9c8587fc08b6a4a097a1e1619d0779b116e25e72
ManiDeep1822 <indlachinnu123@gmail.com>
2026-04-27 09:52:34 +0530

    Fix ReferenceError: useMemo is not defined in TimetableView

--- client/src/pages/TimetableView.jsx
+++ client/src/pages/TimetableView.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useMemo } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { 
   useGetTimetableByIdQuery, 
